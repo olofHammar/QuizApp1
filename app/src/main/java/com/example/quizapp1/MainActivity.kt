b7b66bd13e0927e7db5f0cea4b1e43fce1fe64ee
Olof Hammar <olle_hammat@hotmail.com>
2020-09-24 12:15:14 +0200

    PlayFragment working.

--- app/src/main/java/com/example/quizapp1/MainActivity.kt
+++ app/src/main/java/com/example/quizapp1/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.quizapp1
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +14,6 @@ class MainActivity : AppCompatActivity() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
         loadFragment(HomeFragment())
-
 
         bottom_navigation_view.setOnNavigationItemSelectedListener {
             when(it.itemId){
@@ -33,21 +29,6 @@ class MainActivity : AppCompatActivity() {
             }
             false
         }
-        /*
-        btn_start.setOnClickListener {
-            if (et_name.text.toString().isEmpty()) {
-                Toast.makeText(this, "Skriv in ditt namn", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                val intent = Intent(this, QuizQuestionsActivity::class.java)
-                intent.putExtra(Constants.USER_NAME, et_name.text.toString())
-                startActivity(intent)
-                //stänger aktiviteten vi lämnar.
-                finish()
-            }
-        }
-
-         */
     }
     //Denna funktion skapar fragmentet i mainActivity
     private fun loadFragment(fragment: Fragment) {
--- app/src/main/java/com/example/quizapp1/PlayFragment.kt
+++ app/src/main/java/com/example/quizapp1/PlayFragment.kt
@@ -1,29 +1,35 @@
 package com.example.quizapp1
 
 import android.content.Intent
-import androidx.fragment.app.Fragment
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_play.*
-import kotlinx.android.synthetic.main.fragment_play.view.*
 
 class PlayFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val view: View = inflater.inflate(R.layout.fragment_play,
-        container, false)
-        val btnStart: Button = view.findViewById(R.id.btn_start)
-        btnStart.setOnClickListener {
-            Log.d("!!!","click")
-        }
-        //then return in on create view.
-        return view;
+        val view: View = inflater.inflate(R.layout.fragment_play, container, false)
 
+        val btnStart: Button = view.findViewById(R.id.btn_start)
+
+        btnStart.setOnClickListener {
+            //Log.d("!!!","click")
+            if (et_name.text.toString().isEmpty()) {
+                Toast.makeText(this@PlayFragment.context, "Skriv in ditt namn", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val intent = Intent (this@PlayFragment.context, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                intent.putExtra(Constants.USER_NAME, et_name.text.toString())
+                startActivity(intent)
+            }
+        }
+        return view
     }
 
     }
