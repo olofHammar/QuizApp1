f7b379bff9f11d7260f4d762bd9ead2ed232850c
Olof Hammar <olle_hammat@hotmail.com>
2020-09-24 11:26:56 +0200

    Working click funktion in play_fragment

--- app/src/main/java/com/example/quizapp1/PlayFragment.kt
+++ app/src/main/java/com/example/quizapp1/PlayFragment.kt
@@ -1,16 +1,29 @@
 package com.example.quizapp1
 
+import android.content.Intent
 import androidx.fragment.app.Fragment
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_play.*
+import kotlinx.android.synthetic.main.fragment_play.view.*
 
 class PlayFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_play,container,false)
 
-
+        val view: View = inflater.inflate(R.layout.fragment_play,
+        container, false)
+        val btnStart: Button = view.findViewById(R.id.btn_start)
+        btnStart.setOnClickListener {
+            Log.d("!!!","click")
+        }
+        //then return in on create view.
+        return view;
 
     }
-}
+
+    }
