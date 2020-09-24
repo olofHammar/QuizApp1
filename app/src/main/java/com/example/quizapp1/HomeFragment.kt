e1919301ecb109d127277522d992dd3bce64e557
Olof Hammar <olle_hammat@hotmail.com>
2020-09-24 10:51:53 +0200

    Added listener. Fragments and bottom navigation working. Only simple textView in each fragment.

--- app/src/main/java/com/example/quizapp1/HomeFragment.kt
+++ app/src/main/java/com/example/quizapp1/HomeFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Play.newInstance] factory method to
+ * Use the [Home.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Play : Fragment() {
+class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class Play : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_play, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class Play : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Play.
+         * @return A new instance of fragment Home.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Play().apply {
+            HomeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/quizapp1/MainActivity.kt
+++ app/src/main/java/com/example/quizapp1/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,19 +16,22 @@ class MainActivity : AppCompatActivity() {
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-        var btmNav: BottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        loadFragment(HomeFragment())
 
-        btmNav.setOnNavigationItemSelectedListener { item ->
-            when(item.itemId){
-                R.id.nav_home -> {}
 
-                R.id.nav_play -> {}
+        bottom_navigation_view.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> {loadFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true}
 
-                R.id.nav_high_score -> {true}
+                R.id.nav_play -> {loadFragment(PlayFragment())
+                    return@setOnNavigationItemSelectedListener true}
 
-                else -> {}
+                R.id.nav_high_score -> {loadFragment(HighScoreFragment())
+                    return@setOnNavigationItemSelectedListener true}
+
             }
-            true
+            false
         }
         /*
         btn_start.setOnClickListener {
@@ -44,5 +48,13 @@ class MainActivity : AppCompatActivity() {
         }
 
          */
+    }
+    //Denna funktion skapar fragmentet i mainActivity
+    private fun loadFragment(fragment: Fragment) {
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_layout, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
