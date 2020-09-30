47a132f666d9cf54798929b0d5813b1a597ded40
Olof Hammar <olle_hammat@hotmail.com>
2020-09-30 14:09:15 +0200

    Added buttons to select if you want 10, 20 or 30 questions and synced those in the same way i did with the first one.

--- app/src/main/java/com/example/quizapp1/PlayFragment.kt
+++ app/src/main/java/com/example/quizapp1/PlayFragment.kt
@@ -1,14 +1,18 @@
 package com.example.quizapp1
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 import kotlinx.android.synthetic.main.fragment_play.*
+import java.util.ArrayList
 
 class PlayFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -17,16 +21,32 @@ class PlayFragment : Fragment(){
 
         var totalNrOfQuestions = 0
         val btnStart: Button = view.findViewById(R.id.btn_start)
-        val btnSelect: Button = view.findViewById(R.id.btn_select)
+        val btnSelectTen: Button = view.findViewById(R.id.btn_select_ten)
+        val btnSelectTwenty: Button = view.findViewById(R.id.btn_select_twenty)
+        val btnSelectThirty: Button = view.findViewById(R.id.btn_select_thirty)
 
-        btnSelect.setOnClickListener {
-            btnSelect.setBackgroundResource(R.drawable.round_button_selected)
-            totalNrOfQuestions = 5
+        btnSelectTen.setOnClickListener {
+            defaultButtonView()
+            btnSelectTen.setBackgroundResource(R.drawable.round_button_selected)
+            totalNrOfQuestions = 10
+        }
+        btnSelectTwenty.setOnClickListener {
+            defaultButtonView()
+            btnSelectTwenty.setBackgroundResource(R.drawable.round_button_selected)
+            totalNrOfQuestions = 20
+        }
+        btnSelectThirty.setOnClickListener {
+            defaultButtonView()
+            btnSelectThirty.setBackgroundResource(R.drawable.round_button_selected)
+            totalNrOfQuestions = 30
         }
 
         btnStart.setOnClickListener {
             if (et_name.text.toString().isEmpty()) {
                 Toast.makeText(this@PlayFragment.context, "Skriv in ditt namn", Toast.LENGTH_SHORT).show()
+            }
+            else if (totalNrOfQuestions == 0) {
+                Toast.makeText(this@PlayFragment.context, "Välj hur många frågor du vill svara på", Toast.LENGTH_SHORT).show()
             }
             else {
                 val intent = Intent (this@PlayFragment.context, QuizQuestionsActivity::class.java)
@@ -37,5 +57,15 @@ class PlayFragment : Fragment(){
             }
         }
         return view
+    }
+    private fun defaultButtonView() {
+        val options = ArrayList<TextView>()
+        options.add(0, btn_select_ten)
+        options.add(1, btn_select_twenty)
+        options.add(2, btn_select_thirty)
+
+        for (option in options) {
+            option.setBackgroundResource(R.drawable.round_button)
+        }
     }
     }
