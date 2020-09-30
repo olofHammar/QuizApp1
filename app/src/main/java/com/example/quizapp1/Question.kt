b2f3e3d40b486c89ab4e019fec5b85bf5d13ed57
Olof Hammar <olle_hammat@hotmail.com>
2020-09-30 12:27:27 +0200

    Added button to select number of questions no sync just design

--- app/src/main/java/com/example/quizapp1/Question.kt
+++ app/src/main/java/com/example/quizapp1/Question.kt
@@ -1,6 +1,8 @@
 package com.example.quizapp1
-
-data class Question (
+/*
+(Ändrade från 'data class' till 'class') Här skapar jag en klass som är mallen för alla frågor.
+ */
+class Question (
     val id: Int,
     val question: String,
     val image: Int,
