2c16f97678fbda6d5b14910d257364efdc80147f
Olof Hammar <olle_hammat@hotmail.com>
2020-10-05 12:36:02 +0200

    Added comments to ResultActivity.

--- app/src/main/java/com/example/quizapp1/ResultActivity.kt
+++ app/src/main/java/com/example/quizapp1/ResultActivity.kt
@@ -9,19 +9,27 @@ import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_result.*
 import java.util.prefs.Preferences
-
+//Denna aktivitet visar resultatet av quizet.
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
+        /*
+        Här hämtar jag information från tidigare aktiviteter som sätts till nya variabler i denna aktivitet.
+        Sedan skapar jag en variabel för sharedPreferences.
+         */
         val username = intent.getStringExtra("Constants.USER_NAME")
         tv_name.text = "Grattis ${username?.capitalize()}"
         val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS,0)
         val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS,0)
         val pref = getSharedPreferences("highScore", Context.MODE_PRIVATE)
 
+        /*
+        Om spelarens poäng är högre än någon av tidigar highscore så sparas denna poäng och flyttar ner
+        det tidigare highscoren ett snäpp ner i listan.
+         */
         when  {
             correctAnswers>highScoreOne.playerPoints -> {
                 pref.edit().putString("highScoreThreeName", highScoreTwo.playerName).apply()
