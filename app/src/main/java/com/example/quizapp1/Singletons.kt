ad202a8cc34fc083ab5a7feab87f2ce11e0aff0b
Olof Hammar <olle_hammat@hotmail.com>
2020-10-08 00:56:58 +0200

    Added new design to quiz timer and quiz buttons.

--- app/src/main/java/com/example/quizapp1/Singletons.kt
+++
@@ -1,8 +0,0 @@
-package com.example.quizapp1
-
-object Singletons {
-
-    var highScoreOne: PlayerHighScore = PlayerHighScore()
-    var highScoreTwo: PlayerHighScore = PlayerHighScore()
-    var highScoreThree: PlayerHighScore = PlayerHighScore()
-}
