7917dda400718e3699bf9213a95ea250f4c0267b
Olof Hammar <olle_hammat@hotmail.com>
2020-10-10 14:47:29 +0200

    Added reset to sound-class. Sound randomly dissapears sometimes but so far this seems to work.

--- app/src/main/java/com/example/quizapp1/Sound.kt
+++ app/src/main/java/com/example/quizapp1/Sound.kt
@@ -1,41 +1,82 @@
 package com.example.quizapp1
 
-import android.media.MediaPlayer
 import android.content.Context
+import android.media.MediaPlayer
+import android.media.MediaPlayer.OnCompletionListener
 
 
 class Sound (val context: Context) {
 
     fun clickStandard () {
-        val click = MediaPlayer.create(context, R.raw.click_mouth_pop)
+        var click = MediaPlayer.create(context, R.raw.click_mouth_pop)
+        click.setOnCompletionListener(OnCompletionListener { mp ->
+            mp.reset()
+            mp.release()
+            click = null
+        })
         click.start()
     }
     fun clickSlime () {
-        val clickSlime = MediaPlayer.create(context, R.raw.click_slime_drip)
+        var clickSlime = MediaPlayer.create(context, R.raw.click_slime_drip)
+        clickSlime.setOnCompletionListener(OnCompletionListener { mp ->
+            mp.reset()
+            mp.release()
+            clickSlime = null
+        })
         clickSlime.start()
     }
     fun clickDjungle () {
-        val clickDjunle = MediaPlayer.create(context, R.raw.click_djungle)
+        var clickDjunle = MediaPlayer.create(context, R.raw.click_djungle)
+        clickDjunle.setOnCompletionListener(OnCompletionListener { mp ->
+            mp.reset()
+            mp.release()
+            clickDjunle = null
+        })
         clickDjunle.start()
     }
     fun tickMarimba () {
-        val tickMarimba = MediaPlayer.create(context, R.raw.tick_finished_marimba)
+        var tickMarimba = MediaPlayer.create(context, R.raw.tick_finished_marimba)
+        tickMarimba.setOnCompletionListener(OnCompletionListener { mp ->
+            mp.reset()
+            mp.release()
+            tickMarimba = null
+        })
         tickMarimba.start()
     }
     fun alertRight () {
-        val alertRight = MediaPlayer.create(context, R.raw.alert_right_answer)
+        var alertRight = MediaPlayer.create(context, R.raw.alert_right_answer)
+        alertRight.setOnCompletionListener(OnCompletionListener { mp ->
+            mp.reset()
+            mp.release()
+            alertRight = null
+        })
         alertRight.start()
     }
     fun alertWrong () {
-        val alertWrong = MediaPlayer.create(context, R.raw.alert_wrong_answer)
+        var alertWrong = MediaPlayer.create(context, R.raw.alert_wrong_answer)
+        alertWrong.setOnCompletionListener(OnCompletionListener { mp ->
+            mp.reset()
+            mp.release()
+            alertWrong = null
+        })
         alertWrong.start()
     }
     fun loadPieChart () {
-        val loadPieChart = MediaPlayer.create(context, R.raw.load_piechart)
+        var loadPieChart = MediaPlayer.create(context, R.raw.load_piechart)
+        loadPieChart.setOnCompletionListener(OnCompletionListener { mp ->
+            mp.reset()
+            mp.release()
+            loadPieChart = null
+        })
         loadPieChart.start()
     }
     fun fanfare () {
-        val fanfare = MediaPlayer.create(context, R.raw.fanfare_highscore)
+        var fanfare = MediaPlayer.create(context, R.raw.fanfare_highscore)
+        fanfare.setOnCompletionListener(OnCompletionListener { mp ->
+            mp.reset()
+            mp.release()
+            fanfare = null
+        })
         fanfare.start()
     }
 
