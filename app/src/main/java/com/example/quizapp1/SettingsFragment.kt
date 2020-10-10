c5b0b6651d544260bff11af3e4e02964bf4ab34c
Olof Hammar <olle_hammat@hotmail.com>
2020-10-10 12:19:08 +0200

    Added buttons for sound on and off in settings and added design with push functions.

--- app/src/main/java/com/example/quizapp1/SettingsFragment.kt
+++ app/src/main/java/com/example/quizapp1/SettingsFragment.kt
@@ -14,6 +14,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import kotlinx.android.synthetic.main.activity_quiz_questions.*
 import kotlinx.android.synthetic.main.fragment_play.*
+import kotlinx.android.synthetic.main.fragment_set_nr_of_questions.*
+import kotlinx.android.synthetic.main.fragment_settings.*
 import java.util.ArrayList
 
 class SettingsFragment : Fragment(){
@@ -23,7 +25,9 @@ class SettingsFragment : Fragment(){
 
 
         val click: MediaPlayer = MediaPlayer.create(activity!!.applicationContext, R.raw.click_mouth_pop)
-        var btnBackHome: Button = view.findViewById(R.id.btn_back_home)
+        val btnBackHome: Button = view.findViewById(R.id.btn_back_home)
+        val btnSoundOn: Button = view.findViewById(R.id.btn_sound_on)
+        val btnSoundOff: Button = view.findViewById(R.id.btn_sound_off)
 
         btnBackHome.setOnClickListener {
 
@@ -33,7 +37,29 @@ class SettingsFragment : Fragment(){
             t.replace(R.id.fragment_layout, mFrag)
             t.commit()
         }
+        btnSoundOn.setOnClickListener {
+            click.start()
+            defaultButtonView()
+            btnSoundOn.setBackgroundResource(R.drawable.round_button_selected)
+            btnSoundOn.setTextColor(Color.parseColor("#696969"))
+        }
+        btnSoundOff.setOnClickListener {
+            click.start()
+            defaultButtonView()
+            btnSoundOff.setBackgroundResource(R.drawable.round_button_selected)
+            btnSoundOff.setTextColor(Color.parseColor("#696969"))
+        }
 
         return view
+    }
+    private fun defaultButtonView() {
+        val options = ArrayList<TextView>()
+        options.add(0, btn_sound_on)
+        options.add(1, btn_sound_off)
+
+        for (option in options) {
+            option.setBackgroundResource(R.drawable.round_button)
+            option.setTextColor(Color.parseColor("#888888"))
+        }
     }
 }
